e5b3230a75c61dadd80e929d34cff880a1ac2d5f
yunk._.k <hamndo@naver.com>
2023-11-17 20:45:16 +0900

    [11/17] express

---
+++ 3.NODE/3.express/app2.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World! GET received");
+});
+
+app.post("/", (req, res) => {
+  res.send("Hello, World! POST received");
+});
+
+app.put("/", (req, res) => {
+  res.send("Hello, World! PUT received");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Hello, World! DELETE received");
+});
+
+app.listen(port, () => {
+  console.log(`서버가 ${port} 에 생성 되었습니다.`);
+});
